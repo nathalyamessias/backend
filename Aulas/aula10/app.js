103a808da216b61d8f67ed60c6cae6f1543d2640
Nathalya <Nathalya.messias@iesb.edu.br>
2025-10-16 09:39:04 -0300

    commit pratica06

---
+++ Aulas/aula10/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/`;
+
+mongoose 
+.connect(url)
+.then(()=> console.log("conectado ao MongoDB"))
+.catch((err) => console.log("Erro ao conectar com MongoDB", err.message));
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+module.exports = app;
