a0685d3a6d0270b274595c178e6a9e0bf1b2c41d
Nathalya <Nathalya.messias@iesb.edu.br>
2025-10-23 10:44:51 -0300

    aula10definitiva

--- Aulas/aula10/app.js
+++ Aulas/aula10/app.js
@@ -1,20 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
 const tarefasRouter = require('./routes/tarefasRouter');
 
-const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/`;
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DBNAME}`;
 
-mongoose 
-.connect(url)
-.then(()=> console.log("conectado ao MongoDB"))
-.catch((err) => console.log("Erro ao conectar com MongoDB", err.message));
+mongoose
+  .connect(url)
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.log("Erro ao conectar com MongoDB", err.message));
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- Aulas/aula10/routes/tarefasRouter.js
+++ Aulas/aula10/routes/tarefasRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const controller = require('../controllers/tarefasController');
 
 const router = express.Router();
